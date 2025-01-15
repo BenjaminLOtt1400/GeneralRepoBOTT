a34b3d0ed0755788e7ae76a70581ee0431cbdbdf
BenjaminLOtt1400 <benjaminott@mail.weber.edu>
2025-01-15 12:14:08 -0700

    Silly puzzle

--- 1400FirstTester.py
+++ 1400FirstTester.py
@@ -1,4 +1,48 @@
-conversion_rate = 1.44
-amount = int(input("How many dollars USD would you like to convert to dollars CAN?"))
-total = (amount * conversion_rate)
-print(total)
+
+
+
+
+c0c0 = "_h"
+
+
+a_2_3_4 = "1d"
+
+continue_ = "DE"
+
+NONE = "N_"
+
+false = "fro"
+
+Return = "m"
+
+
+LookAtThis = "_👀_"
+
+
+Continue = "_F" + int(59 * 47 / 3 // float("400")) * "3"
+
+iiiiii = "T_0FT"
+
+eNgLiShOrFrEnCh = "en_"
+
+
+
+this_IS_AVery_goodVariable = "Re5"
+
+this_IS_AVery_BadVariable = "t._L" + 2 * str(0)
+
+camelCase1 = f"k_" + f"b{3:.0f}n" + str(34)
+
+
+newline = '''the_
+des'''
+
+cat5 = "ks_"
+
+theNumberTWO = str(2)
+
+is_thisVariable_correct__whoKnows_notMe = "_so" + int(.0332 // 5) * "4" + "lv" + str(3)
+
+Finally = '_THE 🧩'
+
+print(c0c0, a_2_3_4, continue_, NONE, false, Return, LookAtThis, Continue, iiiiii, eNgLiShOrFrEnCh, this_IS_AVery_goodVariable, this_IS_AVery_BadVariable, camelCase1, newline, cat5, theNumberTWO, is_thisVariable_correct__whoKnows_notMe, Finally    )
